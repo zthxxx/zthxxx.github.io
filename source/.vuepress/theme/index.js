b467028263878a44016ceeff10bdc65eb3e587d3
zthxxx <zthxxx.me@gmail.com>
2019-02-27 12:23:00 +0800

    fix(theme): alter last-updated format

--- source/.vuepress/theme/index.js
+++ source/.vuepress/theme/index.js
@@ -7,6 +7,9 @@ module.exports = themeConfig => ({
     ['@vuepress/blog', {
       permalink: '/post/:slug',
     }],
+    ['@vuepress/last-updated', {
+      transformer: timestamp => new Date(timestamp).toISOString().replace('T', ' ').split(/:\d{2}\./)[0]
+    }],
     '@vuepress/pagination',
     ['@vuepress/search', {
       searchMaxSuggestions: 10,
